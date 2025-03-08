dd62bda8ac99960cbce8efbeb900c49969652b81
mdekerga <mathieu.dekergariou@gmail.com>
2025-03-08 17:46:40 +0100

    gitignore add

---
+++ 800/B_Drinks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n,drink;
+
+    cin >> n;
+    
+    while(n--){
+        cin >> drink;
+
+        cout << drink + " ";
+    }
+}
